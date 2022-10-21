cfd3f016c99e21cc5fe173903da8847cc26d8763
Aidan Quinlan <aidankquinlan@gmail.com>
2022-10-21 13:37:24 -0700

    gallery test

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,16 @@ $(".grid-btn").click(function() {
   $('.grid').toggleClass( "active" );
   $('.grid-btn').toggleClass( "active" );
 });
+
+$(".gallery div img").click(function() {
+
+  if ( $( this ).parent().hasClass( "active" ) ) {
+    $(".gallery div").show();
+    $(this).parent().removeClass( "active" );
+  } else {
+    $(".gallery div").hide();
+    $(this).parent().show();
+    $(this).parent().addClass( "active" );
+  }
+
+});
