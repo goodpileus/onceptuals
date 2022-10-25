7b913a4aa9a89e320fd0d53e7a9b32cbad0cb304
Aidan Quinlan <aidankquinlan@gmail.com>
2022-10-24 18:38:51 -0700

    transcript and links pages, typography updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,50 @@
-$(".grid-btn").click(function() {
-  $('.grid').toggleClass( "active" );
-  $('.grid-btn').toggleClass( "active" );
-});
-
 $(".gallery div img").click(function() {
 
   if ( $( this ).parent().hasClass( "active" ) ) {
-    $(".gallery div").show();
+    $('main').removeClass( "lightbox" );
+    $(".lightbox-bg").removeClass('active');
+
+    $(".gallery div").removeClass('inactive');
     $(this).parent().removeClass( "active" );
   } else {
-    $(".gallery div").hide();
-    $(this).parent().show();
+    $('main').addClass( "lightbox" );
+    $(".lightbox-bg").addClass('active');
+
+    $(".gallery div").addClass('inactive');
+    $(this).parent().removeClass( "inactive" );
     $(this).parent().addClass( "active" );
   }
 
 });
+
+$(".lightbox-bg").click(function() {
+  $('main').removeClass( "lightbox" );
+  $(".lightbox-bg").removeClass('active');
+
+  $(".gallery div").removeClass('inactive');
+  $(".gallery div").removeClass('active');
+});
+
+
+// $(".grid-btn").click(function() {
+//   $('.grid').toggleClass( "active" );
+//   $('.grid-btn').toggleClass( "active" );
+// });
+
+if(Cookies.get('grid') == 'true') {
+  $('.grid').removeClass( "active" );
+  $('.grid-btn').addClass( "active" );
+}
+
+function grid() {
+    $('.grid').toggleClass( "active" );
+    $('.grid-btn').toggleClass( "active" );
+
+  if(Cookies.get('grid') == 'true') {
+    Cookies.set('grid', false);
+    console.log('grid off');
+  } else {
+    Cookies.set('grid', true);
+    console.log('grid on');
+  }
+};
